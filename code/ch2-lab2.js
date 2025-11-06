2f0e91198a1d87b666db767e7c361a3d06cb181f
Josh Furer <joshuamfurer@gmail.com>
2025-11-06 15:57:03 -0500

    Ch2 done

---
+++ code/ch2-lab2.js
@@ -0,0 +1,37 @@
+// Dishes Data
+const dishData = [
+    { name: "Italian pasta", price: 9.55 },
+    { name: "Rice with veggies", price: 8.65 },
+    { name: "Chicken with potatoes", price: 15.55 },
+    { name: "Vegetarian Pizza", price: 6.45 },
+];
+
+// Tax Value
+const tax = 1.20;
+
+// Task 1: Implement getPrices()
+function getPrices(taxBoolean) {
+    for (const dish of dishData) {
+        let finalPrice;
+        if (taxBoolean === true) {
+            finalPrice = dish.price * tax;
+        } else if (taxBoolean === false) {
+            finalPrice = dish.price;
+        } else {
+            console.log("You need to pass a boolean to the getPrices call!");
+            return;
+        }
+        console.log("Dish: ", dish.name, "Price: $", finalPrice);
+    }
+}
+
+// Task 2: Implement getDiscount()
+// function getDiscount(taxBoolean, guests) {
+//     // WRITE YOUR CODE HERE
+// }
+
+// Call getDiscount() with sample arguments
+// getDiscount(true, 2);
+// getDiscount(false, 10);
+
+getPrices(true);
